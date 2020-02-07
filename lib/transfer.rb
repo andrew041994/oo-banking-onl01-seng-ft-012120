119d2944c2f2d9b68dac4a48e059771576e09b90
Alehandro Persaud <andrew041994@gmail.com>
2020-02-07 02:18:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     end
 
     def execute_transaction
-      if sender.valid?
+      if sender.status == "open"
           if  sender.balance > @amount && @status =="pending"
             sender.balance = sender.balance - @amount
             receiver.balance = receiver.balance += @amount
