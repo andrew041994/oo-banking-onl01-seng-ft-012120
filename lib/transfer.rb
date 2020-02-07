30f4a4c3bc144006027ea8d8c46df1cfe40e705b
Alehandro Persaud <andrew041994@gmail.com>
2020-02-07 02:03:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       if  sender.balance > @amount && @status =="pending"
         sender.balance = sender.balance - @amount
         receiver.balance = receiver.balance += @amount
-        @status = "complete"
+        # @status = "complete"
       else
             return "Transaction rejected. Please check your account balance."
       # rejected
